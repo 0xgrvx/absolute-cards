f9d8bb5d1e128a664a295123b6010ebd900ef5a4
Gaurav Mandall <mandalgaurav550@gmail.com>
2026-06-10 05:56:20 +0530

    build: simplify next config for cloudflare pages build and fix dynamic assets
    Co-authored-by: Gaurav <72154932+gauravmandall@users.noreply.github.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: process.env.NODE_ENV === 'production' ? '/absolute-cards' : '',
   output: 'export',
   images: { unoptimized: true },
 };
